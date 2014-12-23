9df3b9bd6b461b8be2dec55b2bf9084555626a69
nstjelja@gmail.com <ns123456>
2014-12-23 11:07:36 +0100

    Complete TwoPairRule

---
+++ PsychicPoker.Test/Engine/Rules/TwoPairRuleTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PsychicPoker.Domain;
+using System.Collections.Generic;
+using PsychicPoker.Engine.Rules;
+using System.Linq;
+
+namespace PsychicPoker.Test.Engine.Rules
+{
+    [TestClass]
+    public class TwoPairRuleTest
+    {
+        [TestMethod]
+        public void CardList_Is_Two_Pairs()
+        {
+            var cardList = new List<Card>() { 
+                new Card(){FaceValue = CardFaceValue.Ace,Suit = CardSuit.Clubs },
+                new Card(){FaceValue = CardFaceValue.Two,Suit = CardSuit.Diamonds },
+                new Card(){FaceValue = CardFaceValue.Jack,Suit = CardSuit.Spades },
+                new Card(){FaceValue = CardFaceValue.Jack,Suit = CardSuit.Diamonds },
+                new Card(){FaceValue = CardFaceValue.Five,Suit = CardSuit.Hearts },
+                new Card(){FaceValue = CardFaceValue.HighAce,Suit = CardSuit.Hearts },
+                new Card(){FaceValue = CardFaceValue.Ten,Suit = CardSuit.Hearts },
+                new Card(){FaceValue = CardFaceValue.Queen,Suit = CardSuit.Spades },
+                new Card(){FaceValue = CardFaceValue.Queen,Suit = CardSuit.Hearts },
+                new Card(){FaceValue = CardFaceValue.King,Suit = CardSuit.Hearts }
+            };
+
+            var rule = new TwoPairRule();
+            var hand = rule.BuildStrongestHand(cardList);
+
+            Assert.IsNotNull(hand);
+            Assert.AreEqual("Two pair", hand.Name);
+            Assert.AreEqual(300, hand.RankPrimary);
+            Assert.IsTrue(hand.Cards.Any(x => x.FaceValue == CardFaceValue.Jack));
+            Assert.IsTrue(hand.Cards.Any(x => x.FaceValue == CardFaceValue.Queen));
+        }
+
+        [TestMethod]
+        public void CardList_Is_Not_Two_Pairs()
+        {
+            var cardList = new List<Card>() { 
+                new Card(){FaceValue = CardFaceValue.Ace,Suit = CardSuit.Clubs },
+                new Card(){FaceValue = CardFaceValue.Two,Suit = CardSuit.Diamonds },
+                new Card(){FaceValue = CardFaceValue.Three,Suit = CardSuit.Spades },
+                new Card(){FaceValue = CardFaceValue.Jack,Suit = CardSuit.Diamonds },
+                new Card(){FaceValue = CardFaceValue.Five,Suit = CardSuit.Hearts },
+                new Card(){FaceValue = CardFaceValue.HighAce,Suit = CardSuit.Hearts },
+                new Card(){FaceValue = CardFaceValue.Ten,Suit = CardSuit.Hearts },
+                new Card(){FaceValue = CardFaceValue.Seven,Suit = CardSuit.Spades },
+                new Card(){FaceValue = CardFaceValue.Queen,Suit = CardSuit.Hearts },
+                new Card(){FaceValue = CardFaceValue.King,Suit = CardSuit.Hearts }
+            };
+
+            var rule = new TwoPairRule();
+            var hand = rule.BuildStrongestHand(cardList);
+
+            Assert.IsNull(hand);
+           
+        }
+    }
+}
---
+++ PsychicPoker/Engine/Rules/TwoPairRule.cs
@@ -0,0 +1,49 @@
+﻿using PsychicPoker.Domain;
+using PsychicPoker.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PsychicPoker.Test.Engine.Rules
+{
+    public class TwoPairRule : Rule
+    {
+        public override Domain.Hand BuildStrongestHand(List<Domain.Card> cards)
+        {
+            var pairs = new Dictionary<List<Card>, int>();
+
+            m_Faces.ForEach(faceValue => {
+                var pair = cards.Where(x => x.FaceValue == faceValue).ToList();
+
+                if (pair.Count != 2)
+                    return;
+
+                pairs.Add(pair, pair.Sum(x => (int)x.FaceValue));
+            });
+
+            if (pairs.Count < 2)
+                return null;
+
+            var bestPairs = pairs.OrderByDescending(x => x.Value).Take(2).Select(x => x.Key).ToList();
+            bestPairs[0].AddRange(bestPairs[1]);
+
+            var faceOne = bestPairs[0][0].FaceValue;
+            var faceTwo = bestPairs[1][0].FaceValue;
+
+            var bestOtherCard = cards.Where(x => x.FaceValue != faceOne && x.FaceValue != faceTwo).OrderByDescending(x => (int)x.FaceValue).First();
+            bestPairs[0].Add(bestOtherCard);
+
+            var hand = new Hand();
+            hand.Name = "Two pair";
+            hand.Cards = bestPairs.First();
+            hand.RankPrimary = 300;
+            hand.RankSecondry = bestPairs.First().Sum(x => (int)x.FaceValue);
+
+            return hand;
+
+           
+        }
+    }
+}
